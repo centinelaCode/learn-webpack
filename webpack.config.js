f06d8f664d5c7fe158171fc7db5003bdac18b590
centinelacode <centinela1024tb@gmail.com>
2022-01-31 16:21:02 -0600

    Se agrega la configuración del plugin(plugins) de html-webpack-plugin para generar el html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
    entry: "./src/index.js",
@@ -19,5 +20,12 @@ module.exports = {
             }
          }
       ]
-   }
+   },"build": "webpack --mode production",
+   plugins: [
+      new htmlWebPackPlugin({
+         inject: true,
+         template: './public/index.html',
+         filename: './index.html'
+      })
+   ]
 }
