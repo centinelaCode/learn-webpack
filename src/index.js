21589f922795b18592c5e2e1dc10c7debdd78488
centinelacode <centinela1024tb@gmail.com>
2022-02-03 13:29:20 -0600

    Se configura el modo watch en webpack.config.dev.js solo agregando watch: true

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Template from '@templates/Template.js';
 import '@styles/main.css';
 // import './styles/vars.styl';
 import '@styles/vars.scss';
-
+console.log('Hola');
 
 (async function App() {
   const main = null || document.getElementById('main');
